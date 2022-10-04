692f09e5877f22f9ad9e416da7bec4cd4ecc7677
Jesus Abraham Valera Valdez <jesus.valera@sonorasoft.com>
2022-10-04 14:46:30 -0700

    Cleaning up the member service

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -50,7 +50,8 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
--- API/Extensions/HttpExtensions.cs
+++ API/Extensions/HttpExtensions.cs
@@ -9,7 +9,11 @@ namespace API.Extensions
         public static void AddPaginationHeader(this HttpResponse response, int currentPage, int itemsPerpage, int totalItems, int totalPages)
         {
             var paginationHeader = new PaginationHeader(currentPage, itemsPerpage, totalItems, totalPages);
-            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader));
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader, options));
             //Expose the headers
             response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
         }
