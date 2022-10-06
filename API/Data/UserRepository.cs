fd6596b557f26ac796c103ef96f8f3e9db26995f
Jesus Abraham Valera Valdez <jesus.valera@sonorasoft.com>
2022-10-06 09:20:55 -0700

    Adding sorting functionality

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -42,6 +42,12 @@ namespace API.Data
 
             query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
 
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(x => x.Created),
+                _ => query.OrderByDescending(x => x.LastActive)
+            };  
+
             return await PagedList<MemberDTO>.CreateAsync(query.ProjectTo<MemberDTO>(_mapper.ConfigurationProvider).AsNoTracking(), userParams.PageNumber, userParams.PageSize);
         }
 
